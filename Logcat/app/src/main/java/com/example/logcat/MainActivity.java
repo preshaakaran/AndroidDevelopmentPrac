77bd25f90ac84b9f56337ff5fcbedad19e9c39fe
Presha Karan <karanpresha@gmail.com>
2024-09-13 17:36:13 +0530

    first commit

---
+++ Logcat/app/src/main/java/com/example/logcat/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.logcat;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Log.d("Test","Test Log");
+
+        String TAG = "First Tag";
+        Log.d(TAG,"My First Log Message");
+    }
+}
