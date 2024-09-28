c61e31e1c9c43e828f3a695e0890dad6ced47d81
Presha Karan <karanpresha@gmail.com>
2024-09-28 12:18:09 +0530

    commit

---
+++ Service/app/src/main/java/com/example/service/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.service;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnStart,btnStop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        btnStart=findViewById(R.id.btnStart);
+        btnStop=findViewById(R.id.btnStop);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(new Intent(MainActivity.this,MusicService.class));
+
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(new Intent(MainActivity.this,MusicService.class));
+
+            }
+        });
+
+
+
+
+
+    }
+}
