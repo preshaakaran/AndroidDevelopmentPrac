4c3d41b5dc1848f00e8dc4b74ba2df03ee2a9e61
Presha Karan <karanpresha@gmail.com>
2024-10-04 00:36:32 +0530

    commit

---
+++ TabLayout/app/src/main/java/com/example/tablayout/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.tablayout;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+    TabLayout tab;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        tab = findViewById(R.id.tab);
+        viewPager = findViewById(R.id.viewPager);
+
+        ViewPagerMessengerAdapter adapter = new ViewPagerMessengerAdapter(getSupportFragmentManager());
+
+        viewPager.setAdapter(adapter);
+        tab.setupWithViewPager(viewPager);
+
+
+
+
+
+
+    }
+}
