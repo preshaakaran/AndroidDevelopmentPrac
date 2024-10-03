8001d3205dea52d2a22210bcb4c8355b0cf01006
Presha Karan <karanpresha@gmail.com>
2024-10-04 00:38:14 +0530

    commit

---
+++ WebView/app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.webview;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    ProgressBar pgBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+        pgBar = findViewById(R.id.pgBar);
+
+        webView.loadUrl("https://www.google.com");
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                pgBar.setVisibility(View.VISIBLE);
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                pgBar.setVisibility(View.GONE);
+                super.onPageFinished(view, url);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }else {
+            super.onBackPressed();
+        }
+    }
+}
